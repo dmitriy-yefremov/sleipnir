dc843a7516312f3b1d9319a7f73513bc9b364fdc
Anirudh Padmarao <apadmarao@linkedin.com>
2014-12-16 09:57:34 -0800

    Update union types to have generation time type checking.

--- generator/src/main/scala/com/linkedin/sleipnir/data/ScalaUnionTemplate.scala
+++ generator/src/main/scala/com/linkedin/sleipnir/data/ScalaUnionTemplate.scala
@@ -1,10 +1,7 @@
 package com.linkedin.sleipnir.data
 
-import scala.reflect.ClassTag
-
-import com.linkedin.data.DataMap
 import com.linkedin.data.schema._
-import com.linkedin.data.template.{DataTemplate, UnionTemplate}
+import com.linkedin.data.template.UnionTemplate
 
 /**
  * A super type for all union types. This class is designed to take care of all heavy lifting and make extending classes
@@ -12,33 +9,5 @@ import com.linkedin.data.template.{DataTemplate, UnionTemplate}
  * @author Dmitriy Yefremov
  */
 abstract class ScalaUnionTemplate protected(data: AnyRef, schema: UnionDataSchema) extends UnionTemplate(data, schema) {
-
-  protected def set[T: ClassTag](memberSchema: DataSchema, value: T): Unit = {
-    val clazz = implicitly[ClassTag[T]].runtimeClass
-    memberSchema.getDereferencedDataSchema match {
-      case _: PrimitiveDataSchema =>
-        selectDirect(memberSchema, clazz.asInstanceOf[Class[T]], memberSchema.getUnionMemberKey, value)
-      case _: RecordDataSchema | _: FixedDataSchema | _: UnionDataSchema =>
-        selectWrapped(memberSchema, clazz.asInstanceOf[Class[DataTemplate[DataMap]]], memberSchema.getUnionMemberKey, value.asInstanceOf[DataTemplate[DataMap]])
-    }
-  }
-
-  protected def get[T: ClassTag](memberSchema: DataSchema): T = {
-    val clazz = implicitly[ClassTag[T]].runtimeClass
-    memberSchema.getDereferencedDataSchema match {
-      case _: PrimitiveDataSchema =>
-        obtainDirect(memberSchema, clazz, memberSchema.getUnionMemberKey).asInstanceOf[T]
-      case _: RecordDataSchema | _: FixedDataSchema | _: UnionDataSchema =>
-        obtainWrapped(memberSchema, clazz.asInstanceOf[Class[DataTemplate[DataMap]]], memberSchema.getUnionMemberKey).asInstanceOf[T]
-    }
-  }
-
-  protected def maybeGet[T: ClassTag](memberSchema: DataSchema): Option[T] = {
-    if (memberIs(memberSchema.getUnionMemberKey)) {
-      Option(get[T](memberSchema))
-    } else {
-      None
-    }
-  }
 
 }

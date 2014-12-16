18ac7f3c7f4e88dc616fcad736f0d764b99f7009
Anirudh Padmarao <apadmarao@linkedin.com>
2014-12-16 10:30:16 -0800

    Minor cleanup for ScalaEnumTemplate.

--- generator/src/main/scala/com/linkedin/sleipnir/data/ScalaEnumTemplate.scala
+++ generator/src/main/scala/com/linkedin/sleipnir/data/ScalaEnumTemplate.scala
@@ -4,16 +4,15 @@ package com.linkedin.sleipnir.data
  * A super type for all enum types
  * @author Anirudh Padmarao
  */
-trait ScalaEnumTemplate extends Enumeration {
+abstract class ScalaEnumTemplate extends Enumeration {
 
+  /**
+   * The [[Value]] from this [[Enumeration]] to use if the string representing the value
+   * during deserialization from Pegasus JSON is not recognized.
+   */
   val $Unknown = Value
 
-  def withNameOrUnknown(s: String): Value = {
-    try {
-      withName(s)
-    } catch {
-      case _: NoSuchElementException => $Unknown
-    }
-  }
+  def withNameOrUnknown(s: String): Value =
+    values.find(_.toString == s).getOrElse($Unknown)
 
 }

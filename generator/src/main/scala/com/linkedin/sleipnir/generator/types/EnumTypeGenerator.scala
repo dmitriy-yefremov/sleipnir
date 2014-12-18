94b3ccd5aeed5249e6ae815b96ff6c61a8b85124
Anirudh Padmarao <apadmarao@linkedin.com>
2014-12-18 11:17:27 -0800

    Fix escaping of names in enum template.

--- generator/src/main/scala/com/linkedin/sleipnir/generator/types/EnumTypeGenerator.scala
+++ generator/src/main/scala/com/linkedin/sleipnir/generator/types/EnumTypeGenerator.scala
@@ -19,7 +19,7 @@ class EnumTypeGenerator(override val schema: EnumDataSchema,
       schema.getName,
       namespace(schema.getNamespace),
       namespace(schema.getFullName),
-      namespace(s"${schema.getFullName}.${schema.getName}Value")
+      namespace(s"${schema.getFullName}.CustomValue")
     )
   })
 
